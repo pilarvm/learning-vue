75bb0d6e6e7d1c58c850e1d97f5b1ac91beef1a4
pilarvm <pjvarillas@gmail.com>
2019-07-04 09:19:35 -0500

    first commit

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo:'Hola mundo con vue',
+        frutas:[
+            {nombre:'Pera', cantidad:0},
+            {nombre:'Manzana', cantidad:5},
+            {nombre:'Platano', cantidad:30}
+        ]
+    }
+})
